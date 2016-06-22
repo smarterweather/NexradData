2ff2528ddb50938039660f9f7e7ad692a560dead
afisch710 <alexander-fischman@uiowa.edu>
2016-06-21 21:50:21 -0700

    Fixed build error.

--- src/weather/Main.java
+++ src/weather/Main.java
@@ -58,6 +58,8 @@ public class Main {
 				System.out.println(attribute.getFullName());
 				Array values = attribute.getValues();
 				
+//				values.
+				
 				System.out.println(values.getDataType().getPrimitiveClassType().getCanonicalName());
 			}
 			
